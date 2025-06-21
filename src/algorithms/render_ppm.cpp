4a9b8cf33f99a97a5900512ed57ab546ebb64566
Christian Schulz <chrissi.schulz@googlemail.com>
2025-06-21 16:05:58 +0200

    Correct cosine sampling, add photon tracing

--- src/algorithms/render_ppm.cpp
+++ src/algorithms/render_ppm.cpp
@@ -18,28 +18,33 @@
 #define OMP_THREAD_NUM() 0
 #endif
 
-struct Photon {
-    rgb contrib;         ///< Path contribution
-    SurfaceParams surf;  ///< Surface parameters at the vertex
-    float3 in_dir;       ///< Incoming direction
+struct Photon
+{
+    rgb contrib;        ///< Path contribution
+    SurfaceParams surf; ///< Surface parameters at the vertex
+    float3 in_dir;      ///< Incoming direction
 
     Photon() {}
-    Photon(const rgb& c, const SurfaceParams& s, const float3& i)
+    Photon(const rgb &c, const SurfaceParams &s, const float3 &i)
         : contrib(c), surf(s), in_dir(i)
-    {}
+    {
+    }
 };
 
-class PhotonMappingRenderer : public Renderer {
+class PhotonMappingRenderer : public Renderer
+{
 public:
-    PhotonMappingRenderer(const Scene& scene, size_t max_path_len)
+    PhotonMappingRenderer(const Scene &scene, size_t max_path_len)
         : Renderer(scene), max_path_len(max_path_len)
-    {}
+    {
+    }
 
     std::string name() const override { return "ppm"; }
 
     void reset() override { iter = 1; }
 
-    void render(Image& img) override {
+    void render(Image &img) override
+    {
         constexpr float alpha = 0.75f;
 
         static float base_radius = 1.0f;
@@ -57,58 +62,66 @@ public:
         std::mutex photon_mutex;
         size_t batch_size = 32;
         parallel_for(0, light_path_count / batch_size + (light_path_count % batch_size ? 1 : 0),
-        [&](size_t batch_id) {
-            std::vector<Photon> photon_buffer;
-            UniformSampler sampler(sampler_seed(batch_id, iter));
+                     [&](size_t batch_id)
+                     {
+                         std::vector<Photon> photon_buffer;
+                         UniformSampler sampler(sampler_seed(batch_id, iter));
 
-            int num = std::min((batch_id + 1) * batch_size, light_path_count) - batch_id * batch_size;
-            for (int i = 0; i < num; ++i)
-                trace_photons(photon_buffer, sampler);
+                         int num = std::min((batch_id + 1) * batch_size, light_path_count) - batch_id * batch_size;
+                         for (int i = 0; i < num; ++i)
+                             trace_photons(photon_buffer, sampler);
 
-            { std::lock_guard guard(photon_mutex);
-                photons.insert(photons.end(), photon_buffer.begin(), photon_buffer.end());
-            }
-        });
+                         {
+                             std::lock_guard guard(photon_mutex);
+                             photons.insert(photons.end(), photon_buffer.begin(), photon_buffer.end());
+                         }
+                     });
 #else
-        #pragma omp parallel
+#pragma omp parallel
         {
             std::vector<Photon> photon_buffer;
             UniformSampler sampler(sampler_seed(OMP_THREAD_NUM(), iter));
 
-            #pragma omp for schedule(dynamic) nowait
+#pragma omp for schedule(dynamic) nowait
             for (size_t i = 0; i < light_path_count; i++)
                 trace_photons(photon_buffer, sampler);
 
-            #pragma omp critical
-            { photons.insert(photons.end(), photon_buffer.begin(), photon_buffer.end()); }
+#pragma omp critical
+            {
+                photons.insert(photons.end(), photon_buffer.begin(), photon_buffer.end());
+            }
         }
 #endif
 
         // Build the photon map
         radius = base_radius / std::pow(float(iter), 0.5f * (1.0f - alpha));
         photon_map.build(
-            [&] (size_t i) { return photons[i].surf.point; },
+            [&](size_t i)
+            { return photons[i].surf.point; },
             photons.size(),
             radius);
 
         // Trace the eye paths
         process_tiles(0, 0, img.width, img.height,
-            default_tile_width, default_tile_height,
-            [&] (size_t xmin, size_t ymin, size_t xmax, size_t ymax) {
-            UniformSampler sampler(sampler_seed(xmin ^ ymin, iter));
-            for (size_t y = ymin; y < ymax; y++) {
-                for (size_t x = xmin; x < xmax; x++) {
-                    auto ray = scene.camera->gen_ray((x + sampler()) * kx - 1.0f, 1.0f - (y + sampler()) * ky);
-                    debug_raster(x, y);
-                    img(x, y) += atomically(rgba(trace_eye_path(ray, sampler, light_path_count), 1.0f));
-                }
-            }
-        });
+                      default_tile_width, default_tile_height,
+                      [&](size_t xmin, size_t ymin, size_t xmax, size_t ymax)
+                      {
+                          UniformSampler sampler(sampler_seed(xmin ^ ymin, iter));
+                          for (size_t y = ymin; y < ymax; y++)
+                          {
+                              for (size_t x = xmin; x < xmax; x++)
+                              {
+                                  auto ray = scene.camera->gen_ray((x + sampler()) * kx - 1.0f, 1.0f - (y + sampler()) * ky);
+                                  debug_raster(x, y);
+                                  img(x, y) += atomically(rgba(trace_eye_path(ray, sampler, light_path_count), 1.0f));
+                              }
+                          }
+                      });
         iter++;
     }
 
-    void trace_photons(std::vector<Photon>& photons, Sampler& sampler);
-    rgb trace_eye_path(Ray ray, Sampler& sampler, size_t light_path_count);
+    void trace_photons(std::vector<Photon> &photons, Sampler &sampler);
+    rgb trace_eye_path(Ray ray, Sampler &sampler, size_t light_path_count);
     float estimate_pixel_size(size_t w, size_t h);
 
 private:
@@ -119,76 +132,157 @@ private:
     float radius;
 };
 
-void PhotonMappingRenderer::trace_photons(std::vector<Photon>& photons, Sampler& sampler) {
-    // TODO: Choose a light to sample from (uniformly) and get an emission sample for it
-    // Hints:
-    // _ Lights are in scene.lights
-    // _ Emission samples can be obtained by calling sample_emission() on a given light
+void PhotonMappingRenderer::trace_photons(std::vector<Photon> &photons, Sampler &sampler)
+{
+    // Choose a light to sample from (uniformly)
+    size_t light_idx = size_t(sampler() * scene.lights.size());
+    auto &light = scene.lights[light_idx];
 
+    // Get emission sample
+    EmissionSample emission = light->sample_emission(sampler);
 
-    // TODO: Create the starting ray from the light sample
-    // Hints:
-    // _ Add an offset to avoid artifacts. The constructor for Ray is: Ray(origin, direction, offset)
-    // _ Initialize the pdf of the path according to the emission sample
-    // _ Initialize the contribution of the path according to the emission sample
-    Ray ray; // TODO: initialize this correctly!
+    // Create starting ray with offset to avoid artifacts
+    Ray ray(emission.pos, emission.dir, offset);
+    float pdf = emission.pdf_area * emission.pdf_dir * (1.0f / scene.lights.size());
+    rgb contrib = emission.intensity / pdf;
 
-    for (size_t path_len = 0; path_len < max_path_len; path_len++) {
+    for (size_t path_len = 0; path_len < max_path_len; path_len++)
+    {
         Hit hit = scene.intersect(ray);
-        if (hit.tri < 0) break;
+        if (hit.tri < 0)
+            break;
 
         auto mat = scene.material(hit);
         auto surf = scene.surface_params(ray, hit);
         auto out = -ray.dir;
-        if (!mat.bsdf) break;
+        if (!mat.bsdf)
+            break;
 
-        // TODO: Implement photon shooting here
-        // Hints:
-        // _ Do not store photons on specular surfaces (bsdf->type() == Bsdf::Type::Specular)
-        // _ Use Russian Roulette to terminate photon paths (take it into account in the pdf)
-        // _ Add photons to the "photons" vector
-        break; // TODO: Remove this
+        if (mat.emitter)
+        {
+            // Direct hits on a light source
+            if (surf.entering)
+            {
+                contrib += mat.emitter->emission(out, hit.u, hit.v).intensity;
+                // auto light_emission = light->emission(out, hit.u, hit.v);
+                // contrib *= light_emission.intensity;
+            }
+        }
+
+        // Sample BSDF for next direction
+        BsdfSample bsdf = mat.bsdf->sample(sampler, surf, out);
+        if (bsdf.pdf <= 0.0f)
+            break;
+
+        // Update contribution and ray
+        contrib *= bsdf.color * std::abs(dot(bsdf.in, surf.face_normal)) / bsdf.pdf;
+        ray = Ray(surf.point, bsdf.in, offset);
+
+        // Russian Roulette
+        if (path_len > 2)
+        {
+            float q = std::min(0.95f, dot(contrib, luminance));
+            if (sampler() > q)
+                break;
+            contrib = contrib * (1.0f / q);
+        }
+
+        // Store photon if surface is not specular
+        if (mat.bsdf->type() != Bsdf::Type::Specular)
+        {
+            photons.push_back(Photon(contrib, surf, out));
+        }
     }
 }
 
-rgb PhotonMappingRenderer::trace_eye_path(Ray ray, Sampler& sampler, size_t light_path_count) {
+rgb PhotonMappingRenderer::trace_eye_path(Ray ray, Sampler &sampler, size_t light_path_count)
+{
     static size_t max_path_len = 10;
-
-    // TODO: Initialize path variables (see Path Tracing assignment)
-    rgb color; // TODO: initialize correctly
+    rgb color(0.0f);
+    float pdf = 1.0f;
 
     ray.tmin = offset;
-    for (size_t path_len = 0; path_len < max_path_len; path_len++) {
+    for (size_t path_len = 0; path_len < max_path_len; path_len++)
+    {
         Hit hit = scene.intersect(ray);
-        if (hit.tri < 0) break;
+        if (hit.tri < 0)
+            break;
 
         auto surf = scene.surface_params(ray, hit);
         auto mat = scene.material(hit);
         auto out = -ray.dir;
+        auto cosTheta = std::abs(dot(out, surf.face_normal));
 
-        // TODO: Handle direct light hits (see Path Tracing assignment)
+        if (auto light = mat.emitter)
+        {
+            // Direct hits on a light source - add the emission
+            if (surf.entering)
+            {
+                auto light_emission = light->emission(out, hit.u, hit.v);
+                color += light_emission.intensity * mat.bsdf->eval(out, surf, out) * cosTheta;
+            }
+        }
 
-        if (!mat.bsdf) break;
+        // Handle direct light hits
+        //        for (auto &light : scene.lights)
+        //        {
+        //            DirectLightingSample light_sample = light->sample_direct(surf.point, sampler);
+        //            if (light_sample.pdf_area <= 0.0f)
+        //                continue;
+        //
+        //            float3 light_dir = normalize(light_sample.pos - surf.point);
+        //            Ray shadow_ray(surf.point, light_dir, offset);
+        //            Hit shadow_hit = scene.intersect(shadow_ray);
+        //            if (shadow_hit.tri < 0 || shadow_hit.tri == hit.tri)
+        //            {
+        //                float cos_theta = std::abs(dot(light_dir, surf.face_normal));
+        //                color += mat.bsdf->eval(light_dir, surf, out) * light_sample.intensity * cos_theta / light_sample.pdf_area;
+        //            }
+        //        }
 
-        // TODO: Do a photon query if the material is not specular, otherwise bounce (as in Path Tracing)
-        // Hints:
-        // _ Photons can be queried from the "photon_map" object. Use the following function call:
-        float3 point; // TODO: initialize correctly
-        photon_map.query(point,
-            [&] (size_t i) { return photons[i].surf.point; },
-            [&] (size_t i, float d2) {
-                const Photon& p = photons[i];
-                // p is the photon, d2 is the squared distance from the point to the photon
-                // TODO: Do something
-            });
-        // _ Use the Epanechnikov filter as the density estimation kernel
-        break; // TODO: Remove this
+        if (!mat.bsdf)
+            break;
+
+        // Do photon query if material is not specular
+        if (mat.bsdf->type() != Bsdf::Type::Specular)
+        {
+
+            photon_map.query(surf.point, [&](size_t i)
+                             { return photons[i].surf.point; }, [&](size_t i, float d2)
+                             {
+                    const Photon& p = photons[i];
+                    float r2 = radius * radius;
+                    if (d2 > r2) return;
+            
+                    // Epanechnikov kernel
+                    float k = 1.0f - d2 / r2;
+                    color += mat.bsdf->eval(p.in_dir, surf, out) * p.contrib * k * cosTheta / (pi * r2 * light_path_count); });
+        }
+
+        // Sample BSDF for next direction
+        BsdfSample bsdf = mat.bsdf->sample(sampler, surf, out);
+        if (bsdf.pdf <= 0.0f)
+            break;
+
+        // Update contribution and ray
+        pdf *= bsdf.pdf;
+        ray = Ray(surf.point, bsdf.in, offset);
+
+        // Russian Roulette
+        if (path_len > 2)
+        {
+            float q = std::min(0.95f, dot(bsdf.color, luminance));
+            if (sampler() > q)
+                break;
+            pdf *= q;
+        }
     }
 
     return color;
 }
 
-float PhotonMappingRenderer::estimate_pixel_size(size_t w, size_t h) {
+float PhotonMappingRenderer::estimate_pixel_size(size_t w, size_t h)
+{
     float total_dist = 0.0f;
     int total_count = 0;
 
@@ -198,7 +292,8 @@ float PhotonMappingRenderer::estimate_pixel_size(size_t w, size_t h) {
     // Compute distance between neighboring pixels in world space,
     // in order to get a good estimate for the initial photon size.
 #ifdef USE_STD_THREAD
-    parallel_for(0, h / 8 + (h % 8 > 0 ? 1 : 0), [&](size_t ybin) {
+    parallel_for(0, h / 8 + (h % 8 > 0 ? 1 : 0), [&](size_t ybin)
+                 {
         size_t y = ybin * 8;
         float d = 0; int c = 0;
         for (size_t x = 0; x < w; x += 8) {
@@ -223,22 +318,28 @@ float PhotonMappingRenderer::estimate_pixel_size(size_t w, size_t h) {
         }
 
         std::atomic_ref<float>(total_dist) += d;
-        std::atomic_ref<int>(total_count) += c;
-    });
+        std::atomic_ref<int>(total_count) += c; });
 #else
-    #pragma omp parallel for
-    for (size_t y = 0; y < h; y += 8) {
-        float d = 0; int c = 0;
-        for (size_t x = 0; x < w; x += 8) {
-            Ray rays[4]; Hit hits[4];
-            for (int i = 0; i < 4; i++) {
+#pragma omp parallel for
+    for (size_t y = 0; y < h; y += 8)
+    {
+        float d = 0;
+        int c = 0;
+        for (size_t x = 0; x < w; x += 8)
+        {
+            Ray rays[4];
+            Hit hits[4];
+            for (int i = 0; i < 4; i++)
+            {
                 rays[i] = scene.camera->gen_ray(
                     (x + (i % 2 ? 4 : 0)) * kx - 1.0f,
                     1.0f - (y + (i / 2 ? 4 : 0)) * ky);
                 hits[i] = scene.intersect(rays[i]);
             }
-            auto eval_distance = [&] (int i, int j) {
-                if (hits[i].tri >= 0 && hits[i].tri == hits[j].tri) {
+            auto eval_distance = [&](int i, int j)
+            {
+                if (hits[i].tri >= 0 && hits[i].tri == hits[j].tri)
+                {
                     d += length((rays[i].org + hits[i].t * rays[i].dir) -
                                 (rays[j].org + hits[j].t * rays[j].dir));
                     c++;
@@ -250,9 +351,9 @@ float PhotonMappingRenderer::estimate_pixel_size(size_t w, size_t h) {
             eval_distance(1, 3);
         }
 
-        #pragma omp atomic
+#pragma omp atomic
         total_dist += d;
-        #pragma omp atomic
+#pragma omp atomic
         total_count += c;
     }
 #endif
@@ -260,6 +361,7 @@ float PhotonMappingRenderer::estimate_pixel_size(size_t w, size_t h) {
     return total_count > 0 ? total_dist / (4 * total_count) : 1.0f;
 }
 
-std::unique_ptr<Renderer> create_ppm_renderer(const Scene& scene, size_t max_path_len) {
+std::unique_ptr<Renderer> create_ppm_renderer(const Scene &scene, size_t max_path_len)
+{
     return std::unique_ptr<Renderer>(new PhotonMappingRenderer(scene, max_path_len));
 }
--- src/random.h
+++ src/random.h
@@ -8,40 +8,45 @@
 #include <algorithm>
 
 /// Local coordinates for shading.
-struct LocalCoords {
-    float3 n;           ///< Normal
-    float3 t;           ///< Tangent
-    float3 bt;          ///< Bitangent
+struct LocalCoords
+{
+    float3 n;  ///< Normal
+    float3 t;  ///< Tangent
+    float3 bt; ///< Bitangent
     LocalCoords() {}
-    LocalCoords(const float3& n, const float3& t, const float3& bt) : n(n), t(t), bt(bt) {}
+    LocalCoords(const float3 &n, const float3 &t, const float3 &bt) : n(n), t(t), bt(bt) {}
 };
 
 /// Generates local coordinates given a normal vector.
-inline LocalCoords gen_local_coords(const float3& n) {
+inline LocalCoords gen_local_coords(const float3 &n)
+{
     // From "Building an Orthonormal Basis, Revisited", Duff et al.
     const float sign = copysignf(1.0f, n.z);
     const float a = -1.0f / (sign + n.z);
     const float b = n.x * n.y * a;
-    float3 t (1.0f + sign * n.x* n.x * a, sign * b, -sign * n.x);
+    float3 t(1.0f + sign * n.x * n.x * a, sign * b, -sign * n.x);
     float3 bt(b, sign + n.y * n.y * a, -n.y);
     return LocalCoords(n, t, bt);
 }
 
 /// Direction sample, from sampling a set of directions.
-struct DirSample {
+struct DirSample
+{
     float3 dir;
     float pdf;
     DirSample() {}
-    DirSample(const float3& d, float p) : dir(d), pdf(p) {}
+    DirSample(const float3 &d, float p) : dir(d), pdf(p) {}
 };
 
 /// Evaluates the probability to sample a direction on a uniform sphere.
-inline float uniform_sphere_pdf() {
+inline float uniform_sphere_pdf()
+{
     return 1.0f / (4.0f * pi);
 }
 
 /// Samples a sphere uniformly.
-inline DirSample sample_uniform_sphere(float u, float v) {
+inline DirSample sample_uniform_sphere(float u, float v)
+{
     const float c = 2.0f * v - 1.0f;
     const float s = std::sqrt(1.0f - c * c);
     const float phi = 2.0f * pi * u;
@@ -52,7 +57,8 @@ inline DirSample sample_uniform_sphere(float u, float v) {
 }
 
 /// Evaluates the probability to sample a direction on a cosine-weighted hemisphere.
-inline float cosine_hemisphere_pdf(float c) {
+inline float cosine_hemisphere_pdf(float c)
+{
     // TODO: "c" is the cosine of the direction.
     // You should return the corresponding pdf.
     return 1.0f; // <--- This is probably incorrect
@@ -61,32 +67,25 @@ inline float cosine_hemisphere_pdf(float c) {
 }
 
 /// Samples a hemisphere proportionally to the cosine with the normal.
-inline DirSample sample_cosine_hemisphere(const LocalCoords& coords, float u, float v) {
+inline DirSample sample_cosine_hemisphere(const LocalCoords &coords, float u, float v)
+{
     // TODO: Sample a direction on the hemisphere using a pdf proportional to cos(theta).
     // The hemisphere is defined by the coordinate system "coords".
     // "u" and "v" are random numbers between [0, 1].
-
-    float r = sqrtf(u);       // radial component
-    float theta = 2.0f * M_PI * v;  // angular component
-
-    float x = r * cosf(theta);
-    float y = r * sinf(theta);
-
-    // Step 2: Compute z so that the vector is on the hemisphere
-    float z = sqrtf(1.0f - x*x - y*y); // This ensures normalization
-
-    // Step 3: Construct direction in local coordinates
-    float3 dir(x, y, z);
-
-    // PDF is cos(theta) / PI, since it's cosine-weighted hemisphere sampling
-    float pdf = z / pi;
-
-    return DirSample(dir, pdf);
-    //return DirSample(float3(1, 0, 0), 1.0f); // <--- This is probably incorrect
+    float theta = std::acos(std::sqrt(u));
+    float phi = 2 * pi * v;
+    float pdf = std::cos(theta) / pi;
+    float x = std::cos(phi) * std::sin(theta);
+    float y = std::sin(phi) * std::sin(theta);
+    float z = std::cos(theta);
+    float3 directionLocal{x, y, z};
+    float3 directionWorld = coords.n * directionLocal.z + coords.t * directionLocal.x + coords.bt * directionLocal.y;
+    return DirSample(normalize(directionWorld), pdf);
 }
 
 /// Evaluates the probability to sample a direction on a power-cosine-weighted hemisphere.
-inline float cosine_power_hemisphere_pdf(float c, float k) {
+inline float cosine_power_hemisphere_pdf(float c, float k)
+{
     // TODO: "c" is the cosine of the direction, and k is the power.
     // You should return the corresponding pdf.
     return (k + 1.0f) / (2.0f * M_PI) * powf(c, k);
@@ -94,13 +93,15 @@ inline float cosine_power_hemisphere_pdf(float c, float k) {
 }
 
 /// Samples a hemisphere proportionally to the cosine lobe spanned by the normal.
-inline DirSample sample_cosine_power_hemisphere(const LocalCoords& coords, float k, float u, float v) {
+inline DirSample sample_cosine_power_hemisphere(const LocalCoords &coords, float k, float u, float v)
+{
     // TODO: Sample a direction on the hemisphere using a pdf proportional to cos(theta)^k.
     // The hemisphere is defined by the coordinate system "coords".
     // "u" and "v" are random numbers between [0, 1].
 
     // Handle degenerate case: if k is infinity (perfect specular), return the normal
-    if (std::isinf(k)) {
+    if (std::isinf(k))
+    {
         float3 dir_local(0, 0, 1);
         return DirSample(dir_local, 1.0f); // PDF is infinite but treated specially in practice
     }
@@ -132,7 +133,8 @@ inline DirSample sample_cosine_power_hemisphere(const LocalCoords& coords, float
 /// Returns the survival probability of a path, given its contribution.
 /// \param c   the contribution of the path
 /// \param max the maximum survival probability allowed
-inline float russian_roulette(const rgb& c, float max = 0.75f) {
+inline float russian_roulette(const rgb &c, float max = 0.75f)
+{
     return std::min(max, dot(c, luminance) * 2.0f);
 }
 
